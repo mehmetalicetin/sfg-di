0f542615d953d498c1e0dc8100bc740491ff4ace
mehmetali.cetin <mehmetali.cetin@i2i-systems.com>
2022-06-29 11:07:02 +0300

    get object from application context by getBean() method. This shows that how to spring works with dependency injection.

--- src/main/java/com/cetin/education/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/cetin/education/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,6 @@ import com.cetin.education.springframework.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
@@ -16,6 +15,8 @@ public class SfgDiApplication {
 		//spring context içerisinde myController'i arıyor. @Controller ile spring context içerisine dahil etmiştik.
 		String greetings   = myController.sayHello();
 		System.out.println(greetings);
+		//I'm not a creating new object there, I'm asking to spring provide me object there.
+		//This is fundamental tenants of Dependency Injection is to have framework doing.
 	}
 
 }
